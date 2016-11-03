bdd96f00d48d172041fa3396e4136c8c9b086c69
Donnie Flood <donaldflood@gmail.com>
2016-11-03 10:16:12 -0600

    updated package.json with scoped name and git repo. added requires of data models and module.exported them

---
+++ data-model/index.js
@@ -0,0 +1,17 @@
+var Availability = require('./src/Availability')
+var CareAge = require('./src/CareAge')
+var CareExperience = require('./src/CareExperience')
+var CareLocation = require('./src/CareLocation')
+var CareTraining = require('./src/CareTraining')
+var Demeanor = require('./src/Demeanor')
+var Neighborhood = require('./src/Neighborhood')
+
+module.exports = {
+  Availability: Availability,
+  CareAge: CareAge,
+  CareExperience: CareExperience,
+  CareLocation: CareLocation,
+  CareTraining: CareTraining,
+  Demeanor: Demeanor,
+  Neighborhood: Neighborhood
+}
