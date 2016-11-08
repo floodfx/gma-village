7887a1222564484df2d89cae55b17fa54288d5cd
Donnie Flood <donaldflood@gmail.com>
2016-11-08 08:31:13 -0700

    graphql skeleton with schema and stubbed root

---
+++ graphql/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema, Source } = require('graphql');
+var fs = require('fs')
+var {root} = require('./src/graphql-root')
+
+// Construct a schema, using GraphQL schema language
+
+const schemaFile = fs.readFileSync("./src/schema.graphql")
+
+var schema = buildSchema(new Source(schemaFile));
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(8080);
+console.log('Running a GraphQL API server at localhost:8080/graphql');
---
+++ graphql/src/graphql-root.js
@@ -0,0 +1,14 @@
+
+const root = {
+  gma: (id) => {
+    console.log("gma", id)
+  },
+  gmas: (filter) => {
+    console.log("gmas", filter)
+  },
+  saveGma: (input) => {
+    console.log("saveGma", input)
+  }
+}
+
+module.exports = root
