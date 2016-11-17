d84f6028500bad2c1518b83699b7fc69a784176d
Donnie Flood <donaldflood@gmail.com>
2016-11-16 19:15:12 -0700

    redirect static requests for gmas to root

--- react-app/server.js
+++ react-app/server.js
@@ -4,6 +4,11 @@ var app = express();
 // Use the built-in express middleware for serving static files from './public'
 app.use('/', express.static('build'));
 
+// redirect to static react app
+app.get('/gmas', (req, res) => {
+  res.redirect('/');
+});
+
 // Start the server
 var server = app.listen(process.env.PORT || '8080', function () {
   console.log('App listening on port %s', server.address().port);
