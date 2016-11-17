60404f87422bed8fdb46936aef506ba78efa3287
Donnie Flood <donaldflood@gmail.com>
2016-11-16 19:40:57 -0700

    hard coded correct graphql server location for prod

--- react-app/src/graphql/client.js
+++ react-app/src/graphql/client.js
@@ -1,10 +1,11 @@
 import Lokka from 'lokka';
 import Transport from 'lokka-transport-http';
 
-var url = process.env.NODE_ENV === 'production' ? 'http://gma-village-graphql.appspot.com/graphql' : 'http://localhost:8080/graphql'
-const client = new Lokka({
-  transport: new Transport(url, {credentials: false}),
+const prod = process.env.NODE_ENV === 'production'
+var url = prod ? 'https://gma-village-graphql-dev-dot-gma-village.appspot.com/graphql' : 'http://localhost:8080/graphql'
 
+const client = new Lokka({
+  transport: new Transport(url, {credentials: false})
 });
 
 export default client
