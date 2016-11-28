058f12090df4a6334521cc3cac3ff3ed6924b9d7
Donnie Flood <donaldflood@gmail.com>
2016-11-28 12:51:57 -0700

    added basic auth for accessing app. config.js is ignored by git for securing the username and password

--- react-app/server.js
+++ react-app/server.js
@@ -1,8 +1,33 @@
 var express = require('express');
+var basicAuth = require('basic-auth');
+var config = require('./config') // file not in source control for security
 var app = express();
 
+var UN = config.get('GMA_AUTH_USERNAME');
+var PW = config.get('GMA_AUTH_PASSWORD');
+
+// use basic auth to protect the page
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === UN && user.pass === PW) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
 // Use the built-in express middleware for serving static files from './public'
-app.use('/', express.static('build'));
+app.use('/', auth, express.static('build'));
 
 // redirect to static react app
 app.get('/gmas', (req, res) => {
