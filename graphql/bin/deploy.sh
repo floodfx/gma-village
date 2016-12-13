68415a0f8d1dd3ce53ff470a8305ee1878d9f377
Donnie Flood <donaldflood@gmail.com>
2016-12-12 21:19:07 -0700

    allow specification of AK_APP_SECRET on deploy which will write to temp app.yaml for deployment

--- graphql/bin/deploy.sh
+++ graphql/bin/deploy.sh
@@ -3,5 +3,10 @@
 # make sure we are using gma-village project
 gcloud config configurations activate gma-village
 
+AK_APP_SECRET=$1
+
+tempfile=`mktemp`
+sed "s/YOUR_AK_APP_SECRET/${AK_APP_SECRET}/g" app.yaml > $tempfile
+
 # deploy dev app config
-gcloud app deploy
+gcloud app deploy $tempfile
