2a282de98073fd6d4a66bd52e6a4fe403f200794
Donnie Flood <donaldflood@gmail.com>
2017-01-17 23:05:21 -0700

    added button on parent profile to create care need

--- react-app/src/components/ParentProfile.js
+++ react-app/src/components/ParentProfile.js
@@ -12,7 +12,7 @@ class ParentProfile extends Component {
           {parent.profilePhotoUrl &&
             <img className="img-rounded img-responsive gma-orange-border" style={{ objectFit: 'cover', maxWidth: '135px', maxHeight: '135px', minWidth: '135px', minHeight: '135px' }} src={parent.profilePhotoUrl} alt="profile" />
           }
-          <div className="">
+          <div className="tc">
             <h3 className="gma-orange tc gma-font fw3">
               <Link to={`/parent/edit/${parent.id}`}>Parent {parent.first_name} {parent.last_name}</Link>
             </h3>
@@ -21,7 +21,8 @@ class ParentProfile extends Component {
             </h4>
             <h4 className="tc fw4">
               Status: {parent.active ? "Active" : "Not Active"} 
-            </h4>            
+            </h4>
+            <Link to={`/careNeed/create/${parent.id}`} className="btn gma-orange-bg">Post Care Request</Link>            
           </div>
         </div>
       </div>
