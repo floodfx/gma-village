bab98d1157e28b21d3d11b9b51a98b0914dc2509
Donnie Flood <donaldflood@gmail.com>
2017-01-18 07:58:00 -0700

    added AWS env vars

--- graphql/bin/deploy.sh
+++ graphql/bin/deploy.sh
@@ -5,10 +5,12 @@ gcloud config configurations activate gma-village
 
 STAGE=$1
 AK_APP_SECRET=$2
+SNS_AWS_ACCESS_KEY=$3
+SNS_AWS_SECRET_KEY=$4
 
 tempfile=`mktemp`
 uuid=`uuidgen`
-sed "s/YOUR_AK_APP_SECRET/${AK_APP_SECRET}/g; s/STAGE/${STAGE}/g; s/UUID/${uuid}/g" app.yaml > app-prod.yaml
+sed "s/YOUR_AK_APP_SECRET/${AK_APP_SECRET}/g; s/STAGE/${STAGE}/g; s/UUID/${uuid}/g; s/YOUR_AWS_ACCESS_KEY/${SNS_AWS_ACCESS_KEY}/g; s/YOUR_AWS_SECRET_KEY/${SNS_AWS_SECRET_KEY}/g" app.yaml > app-prod.yaml
 
 # deploy dev app config
 gcloud app deploy app-prod.yaml
