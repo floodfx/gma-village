24128d71f539e4eaf4683e764a3fb8a7c88a0466
Donnie Flood <donaldflood@gmail.com>
2017-01-17 23:46:50 -0700

    better scripted deployment

--- react-app/bin/deploy_aws.sh
+++ react-app/bin/deploy_aws.sh
@@ -4,12 +4,25 @@
 npm run build
 
 # upload to s3
+echo "Uploading build to S3"
 aws s3 sync build/ s3://gmavillage-web --profile gmavillage
+echo "Upload complete"
 
-# create invalidation for cloud front
-aws cloudfront create-invalidation \
+#create invalidation for cloud front
+echo "Invalidating CloudFront Distro"
+invalidationId=$(aws cloudfront create-invalidation \
   --distribution-id E1723CTSEUKZBP \
   --paths "/*" \
-  --profile gmavillage
+  --profile gmavillage \
+  --query 'Invalidation.Id')
 
-echo "Now run this: aws cloudfront wait invalidation-completed --distribution-id E1723CTSEUKZBP --id INVALIDATIONID --profile gmavillage"
+# remove leading and trailing quotes
+invalidationId=$(sed -e 's/^"//' -e 's/"$//' <<< $invalidationId)
+
+echo "Waiting for invalidation to complete... (Could take a while)"
+# wait for resolution
+aws cloudfront wait invalidation-completed \
+  --distribution-id E1723CTSEUKZBP \
+  --id ${invalidationId} \
+  --profile gmavillage
+echo "Done"
