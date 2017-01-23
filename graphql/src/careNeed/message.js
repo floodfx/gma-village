5de35b2ee97e038e42ee5997fcf90c7a70fa4d18
Donnie Flood <donaldflood@gmail.com>
2017-01-23 11:05:18 -0700

    changed sms based on feedback. rounding kids ages. tests.  closes #25

--- graphql/src/careNeed/message.js
+++ graphql/src/careNeed/message.js
@@ -9,6 +9,15 @@ const formatNeighborhood = (neighborhood, otherNeighborhood) => {
   }
 }
 
+const formatPhone = (phone) => {
+  if(phone.length === 10) {
+    return `${phone.slice(0,3)}-${phone.slice(3,6)}-${phone.slice(6)}`
+  }
+  else {
+    return phone;
+  }
+}
+
 const ageFromBirthday = (birthday) => {
   var now = moment();
   var bday = moment.unix(birthday);
@@ -16,18 +25,28 @@ const ageFromBirthday = (birthday) => {
   var years = Math.abs(Math.floor(monthsDiff/12));
   var months = Math.abs(monthsDiff % 12);
   var formattedComponents = [];
-  if(years === 1) {
-    formattedComponents.push("1 yr")
-  } else if(years > 1) {
-    formattedComponents.push(`${years} yrs`)
+  roundAgeFormat = (years, months) => {
+    if(years >= 4) {
+      return `${years} yrs old`
+    }
+    var mos = Math.max(months, 1);
+    var moOrMos = months > 1 ? "mos" : "mo";
+    if(years === 0) {
+      return `${mos} ${moOrMos} old`
+    }
+    var yrOrYrs = years > 1 ? 'yrs' : 'yr'
+    if(months >= 0 && months < 4) {
+      return `~${years} ${yrOrYrs} old`
+    }
+    else if(months >= 4 && months < 9) {
+      return `~${years} 1/2 ${yrOrYrs} old`
+    }
+    else if(months >=9) {
+      return `~${years+1} ${(yrOrYrs === 'yr') ? 'yrs' : 'yrs'} old`
+    }
   }
-  if(months === 1) {
-    formattedComponents.push("1 mo")
-  } else if(months > 1) {
-    formattedComponents.push(`${months} mos`)
-  }
-  
-  var text = formattedComponents.join(" & ")+" old";
+   
+  var text = roundAgeFormat(years, months)
   return {
     years,
     months,
@@ -43,28 +62,35 @@ const kidsCount = (kids) => {
   }
 }
 
+const kidsAges = (kids) => {
+  return kids.map((kid) => {
+    var age = ageFromBirthday(kid.birthday);
+    return age.text;
+  }).join(", ")
+}
+
 const buildMessage = (
   parent, 
   kids, 
   neighborhood, 
   otherNeighborhood, 
   startDateTimeOfNeed,
-  endDateTimeOfNeed) => {
-  var kidsAges = kids.map((kid) => {
-    var age = ageFromBirthday(kid.birthday);
-    return age.text;
-  }).join(", ")
+  endDateTimeOfNeed) => {  
   var msg = 
-    `[GmaVillage] Parent ${parent.first_name} `+
-    `needs child care for ${kidsCount(kids)} (${kidsAges}) in `+
+    `Gma Village Parent ${parent.first_name} `+
+    `needs care for ${kidsCount(kids)} (${kidsAges(kids)}) in `+
     `${formatNeighborhood(neighborhood, otherNeighborhood)} `+
     `on ${moment.unix(startDateTimeOfNeed).format("MMM Do, h:mma")}` +
     `-${moment.unix(endDateTimeOfNeed).format("h:mma")}. `+
-    `Text [${parent.phone}] to setup interview.`
+    `Text ${formatPhone(parent.phone)} to setup interview.`
   return msg;
 }
 
 module.exports = {
   buildMessage,
-  ageFromBirthday
+  ageFromBirthday,
+  formatPhone,
+  formatNeighborhood,
+  kidsCount,
+  kidsAges
 }
---
+++ graphql/test/message_test.js
@@ -0,0 +1,121 @@
+var assert = require('assert');
+var { 
+  ageFromBirthday, 
+  formatPhone,
+  formatNeighborhood,
+  buildMessage,
+  kidsCount,
+  kidsAges
+} = require('../src/careNeed/message');
+var moment = require('moment');
+var { Neighborhood } = require('gma-village-data-model');
+
+describe('CareNeed', function() {
+  describe('ageFromBirthday', function() {
+    it('should round ages', function() {
+      var lessThanYear = moment().subtract(7, 'months').unix();
+      var ageInfo = ageFromBirthday(lessThanYear)
+      assert.equal(ageInfo.text, "7 mos old")
+
+      var yearPlus = moment().subtract(1, 'year').subtract(3, 'months').unix()
+      ageInfo = ageFromBirthday(yearPlus)
+      assert.equal(ageInfo.text, "~1 yr old")
+
+      var almost2 = moment().subtract(1, 'year').subtract(10, 'months').unix()
+      ageInfo = ageFromBirthday(almost2)
+      assert.equal(ageInfo.text, "~2 yrs old")
+
+      var twoPoint25 = moment().subtract(2, 'year').subtract(3, 'months').unix()
+      ageInfo = ageFromBirthday(twoPoint25)
+      assert.equal(ageInfo.text, "~2 yrs old")
+
+      var twoPoint5ish = moment().subtract(2, 'year').subtract(4, 'months').unix()
+      ageInfo = ageFromBirthday(twoPoint5ish)
+      assert.equal(ageInfo.text, "~2 1/2 yrs old")
+
+      var twoPoint5ish2 = moment().subtract(2, 'year').subtract(8, 'months').unix()
+      ageInfo = ageFromBirthday(twoPoint5ish2)
+      assert.equal(ageInfo.text, "~2 1/2 yrs old")
+
+      var almost3 = moment().subtract(2, 'year').subtract(10, 'months').unix()
+      ageInfo = ageFromBirthday(almost3)
+      assert.equal(ageInfo.text, "~3 yrs old")
+
+      var almost3 = moment().subtract(3, 'year').subtract(1, 'months').unix()
+      ageInfo = ageFromBirthday(almost3)
+      assert.equal(ageInfo.text, "~3 yrs old")
+
+      var five = moment().subtract(5, 'years').unix()
+      ageInfo = ageFromBirthday(five)
+      assert.equal(ageInfo.text, "5 yrs old")
+
+      var fivePt5 = moment().subtract(5, 'years').subtract('6', 'months').unix()
+      ageInfo = ageFromBirthday(fivePt5)
+      assert.equal(ageInfo.text, "5 yrs old")
+
+      var almost6 = moment().subtract(5, 'years').subtract('11', 'months').unix()
+      ageInfo = ageFromBirthday(almost6)
+      assert.equal(ageInfo.text, "5 yrs old")
+
+      var six = moment().subtract(6, 'years').subtract('4', 'months').unix()
+      ageInfo = ageFromBirthday(six)
+      assert.equal(ageInfo.text, "6 yrs old")
+    })
+  })
+})
+
+describe('CareNeed', function() {
+  describe('formatPhone', function() {
+    it('format phone', function() {
+      var phone = "4441112222";
+      var formattedPhone = formatPhone(phone)
+      assert.equal(formattedPhone, "444-111-2222")
+      phone = "123-444-1133"
+      formattedPhone = formatPhone(phone)
+      assert.equal(formattedPhone, "123-444-1133")
+    })
+  })
+})
+
+describe('CareNeed', function() {
+  describe('kidsCount', function() {
+    it('formats correctly', function() {
+      var kids = [
+        {birthday: 1}
+      ]  
+      var kc = kidsCount(kids);    
+      assert.equal(kc, "1 kid")
+      kids.push({birthday: 2})
+      kc = kidsCount(kids);    
+      assert.equal(kc, "2 kids")
+    })
+  })
+})
+
+describe('CareNeed', function() {
+  describe('message', function() {
+    it('should be', function() {
+      var parent = {
+        first_name: "Donnie",
+        phone: "4441113333"
+      }
+      var kids = [
+        {birthday: moment().subtract(1, 'year').subtract(10, 'months').unix()},
+        {birthday: moment().subtract(3, 'year').subtract(10, 'months').unix()}
+      ]
+      var neighborhood = Neighborhood.NORTH_OAKLAND.name;
+      var otherNeighborhood = "";
+      var startDateTimeOfNeed = moment().add(1, 'day').add(3, 'hours').unix()
+      var endDateTimeOfNeed = moment().add(1, 'day').add(7, 'hours').unix()
+      var targetMessage = `Gma Village Parent ${parent.first_name} `+
+        `needs care for ${kidsCount(kids)} (${kidsAges(kids)}) in `+
+        `${formatNeighborhood(neighborhood, otherNeighborhood)} `+
+        `on ${moment.unix(startDateTimeOfNeed).format("MMM Do, h:mma")}` +
+        `-${moment.unix(endDateTimeOfNeed).format("h:mma")}. `+
+        `Text ${formatPhone(parent.phone)} to setup interview.`
+      var builtMessage = buildMessage(parent, kids, neighborhood, "", startDateTimeOfNeed, endDateTimeOfNeed);
+      assert.equal(targetMessage, builtMessage)
+    })
+  })
+})
+
