622ac9fefef6cad338639ed39da979e7e56a2f0d
Donnie Flood <donaldflood@gmail.com>
2017-04-19 14:46:43 -0600

    account kit init and 404 working with tests

--- java-lambda/src/main/java/com/gmavillage/lambda/accountkit/LambdaHandler.java
+++ java-lambda/src/main/java/com/gmavillage/lambda/accountkit/LambdaHandler.java
@@ -1,5 +1,10 @@
 package com.gmavillage.lambda.accountkit;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static java.lang.System.getenv;
+
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.LambdaProxyEvent;
@@ -7,8 +12,14 @@ import com.amazonaws.services.lambda.runtime.LambdaProxyOutput;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.gmavillage.lambda.util.CORS;
 import com.gmavillage.lambda.util.DebugHelper;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
 
 public class LambdaHandler implements RequestHandler<LambdaProxyEvent, LambdaProxyOutput>, CORS {
+
+  private static final String APP_ID = firstNonNull(getenv("AK_APP_ID"), "AK_APP_ID");
+  private static final String CSRF = firstNonNull(getenv("CSRF"), "CSRF");
+  private static final String VERSION = firstNonNull(getenv("AK_APP_VERSION"), "AK_APP_VERSION");
 
   private void log(final LambdaLogger logger, final String text) {
     if (logger != null) {
@@ -23,16 +34,30 @@ public class LambdaHandler implements RequestHandler<LambdaProxyEvent, LambdaPro
       logger = context.getLogger();
     }
     log(logger, "Received event:" + DebugHelper.toStringLambdaProxyEvent(event));
-    String body = "Hello: ";
-    switch (event.getHttpMethod()) {
-      case "GET":
-        body += "GET";
-        break;
+    final String proxyPath = firstNonNull(event.getPathParameters().get("proxy"), "");
+    switch (proxyPath) {
+      case "init":
+        return success(initAccountKit(event, context));
       default:
-        body += "Default";
-
+        return error404();
     }
+  }
+
+  private String initAccountKit(final LambdaProxyEvent event, final Context context) {
+    final Map<String, String> akparams = Maps.newHashMap();
+    akparams.put("appId", APP_ID);
+    akparams.put("csrf", CSRF);
+    akparams.put("version", VERSION);
+    return new Gson().toJson(akparams);
+  }
+
+  private LambdaProxyOutput success(final String body) {
     return new LambdaProxyOutput(200, CORS.HEADERS, body);
   }
+
+  private LambdaProxyOutput error404() {
+    return new LambdaProxyOutput(404);
+  }
+
 
 }
--- java-lambda/src/test/java/com/gmavillage/lambda/accountkit/LambdaHandlerTest.java
+++ java-lambda/src/test/java/com/gmavillage/lambda/accountkit/LambdaHandlerTest.java
@@ -2,39 +2,59 @@ package com.gmavillage.lambda.accountkit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.amazonaws.services.lambda.runtime.LambdaProxyEvent;
 import com.amazonaws.services.lambda.runtime.LambdaProxyOutput;
-import com.gmavillage.lambda.util.DebugHelper;
 import com.google.common.io.CharStreams;
 import com.google.gson.Gson;
 
 public class LambdaHandlerTest {
 
-  LambdaProxyEvent event;
+  LambdaProxyEvent accountKitInitSuccess;
+  LambdaProxyEvent accountKit404;
 
   @Before
   public void loadEvent() throws Exception {
-    final String json = CharStreams.toString(new InputStreamReader(
-        this.getClass().getClassLoader().getResourceAsStream("test_LambdaProxyEvent.json")));
-    System.out.println("JSON" + json);
-    final Gson gson = new Gson();
-    event = gson.fromJson(json, LambdaProxyEvent.class);
+    accountKitInitSuccess = loadJsonFile("test_LambdaProxyEvent_accountkit_init_success.json");
+    accountKit404 = loadJsonFile("test_LambdaProxyEvent_accountkit_404.json");
   }
 
   @Test
-  public void test() {
+  public void testAccoutKitInitSuccess() {
     final LambdaHandler handler = new LambdaHandler();
-    final LambdaProxyOutput out = handler.handleRequest(event, null);
-    System.out.println("Out" + DebugHelper.toStringLambdaProxyOutput(out));
+    final LambdaProxyOutput out = handler.handleRequest(accountKitInitSuccess, null);
+    // System.out.println("Out" + DebugHelper.toStringLambdaProxyOutput(out));
     assertNotNull(out);
-    assertEquals("Hello: GET", out.getBody());
+    assertTrue(out.getBody().length() > 0);
+    final Gson gson = new Gson();
+    @SuppressWarnings("unchecked")
+    final Map<String, String> json = gson.fromJson(out.getBody(), Map.class);
+    assertEquals(json.get("appId"), "AK_APP_ID");
+    assertEquals(json.get("csrf"), "CSRF");
+    assertEquals(json.get("version"), "AK_APP_VERSION");
+  }
 
+  @Test
+  public void testAccoutKit404() {
+    final LambdaHandler handler = new LambdaHandler();
+    final LambdaProxyOutput out = handler.handleRequest(accountKit404, null);
+    // System.out.println("Out" + DebugHelper.toStringLambdaProxyOutput(out));
+    assertNotNull(out);
+    assertEquals(404, out.getStatusCode());
+
+  }
+
+  private LambdaProxyEvent loadJsonFile(final String name) throws Exception {
+    final String json = CharStreams.toString(
+        new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(name)));
+    return new Gson().fromJson(json, LambdaProxyEvent.class);
   }
 
 }
